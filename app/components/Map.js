bb2535f76d8a45b2ab4944c812abe168f64d1261
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-27 08:28:18 -0800

    Hook up Map component to index.android.js

---
+++ app/components/Map.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  StyleSheet,
+  View
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default class Map extends Component {
+  render() {
+    const { region } = this.props;
+    console.log(region);
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+       >
+       </MapView>
+      </View>
+    );
+  }
+}
