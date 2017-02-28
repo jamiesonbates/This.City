d1ea018ac6b5e58aa9d87920c7bcba5dab1b5a75
Minh Chau <xiu.chau@gmail.com>
2017-02-28 15:23:49 -0800

    Reformat login and registration pages

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Button,
+  Image,
   Text,
   TextInput,
   TouchableHighlight,
@@ -12,35 +13,44 @@ import axios from 'axios';
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 const styles = StyleSheet.create({
-  pageContainer: {
-    alignItems: 'center',
+  heroContainer: {
+    flexDirection: 'column',
+    height: 200,
+    marginBottom: 25
+  },
+  heroImage: {
     flex: 1,
+    height: null,
+    resizeMode: 'cover',
+    width: null
+  },
+  formContainer: {
+    alignItems: 'center',
     flexDirection: 'column',
   },
   icon: {
-    paddingRight: 15
+    paddingRight: 15,
   },
   inputField: {
-    fontSize: 25,
+    fontSize: 17,
     height: 50,
     width: 270
   },
   inputRow: {
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginBottom: 10
   },
-  submitButton: {
-    borderRadius: 16,
-    // borderWidth: 1,
-    height: 44,
-    // justifyContent: 'center',
-    marginTop: 20,
-    width: 80
+  linkBox: {
+    flexDirection: 'row',
+    height: 50,
+    justifyContent: 'center',
+    marginTop: 30,
+    width: 300
   },
   registerLink: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 20
+    color: 'lightcoral',
+    fontSize: 18,
   }
 });
 
@@ -95,46 +105,50 @@ export default class Login extends Component {
   }
 
   render() {
-    return <View style={styles.pageContainer}>
-      <View style={styles.inputRow}>
-        {userIcon}
-        <TextInput
-          name="email"
-          style={styles.inputField}
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
+    return <View style={{ flexDirection: 'column' }}>
+      <View style={styles.heroContainer}>
+        <Image style={styles.heroImage} source={require('../images/hero.jpg')} />
+      </View>
+
+      <View style={styles.formContainer}>
+        <View style={styles.inputRow}>
+          {userIcon}
+          <TextInput
+            name="email"
+            style={styles.inputField}
+            onChangeText={(email) => this.setState({email})}
+            value={this.state.email}
+          />
+        </View>
+
+        <View style={styles.inputRow}>
+          {passwordIcon}
+          <TextInput
+            name="password"
+            style={styles.inputField}
+            onChangeText={(password) => this.setState({password})}
+            secureTextEntry={true}
+            value={this.state.password}
+          />
+        </View>
+
+        <View style={{ marginTop: 25 }}></View>
+
+        <Button
+          color="#517cc6"
+          onPress={this.callApi}
+          style={styles.submitButton}
+          title="Submit"
         />
+
+        <View style={styles.linkBox}>
+          <Text style={{ color: '#393836', fontSize: 18 }}>Not a member?</Text>
+          <TouchableHighlight onPress={() => this.navigate('registration')}>
+            <Text style={styles.registerLink}> Register for free.</Text>
+          </TouchableHighlight>
+        </View>
+
       </View>
-
-      <View style={styles.inputRow}>
-        {passwordIcon}
-        <TextInput
-          name="password"
-          style={styles.inputField}
-          onChangeText={(password) => this.setState({password})}
-          secureTextEntry={true}
-          value={this.state.password}
-        />
-      </View>
-
-      <Button
-        color="#851515"
-        onPress={this.callApi}
-        style={styles.submitButton}
-        title="Submit"
-      />
-
-
-      <View style={styles.registerLink}>
-        <Text>Not a member?</Text>
-        <TouchableHighlight onPress={() => {
-          this.navigate('registration');
-          this.callApi
-        }}>
-          <Text style={{borderWidth: 1, color: 'lightcoral', height: 20}}> Register for free!</Text>
-        </TouchableHighlight>
-      </View>
-
     </View>
   }
 }
--- app/components/Registration.js
+++ app/components/Registration.js
@@ -1,40 +1,57 @@
 import React, { Component } from 'react';
 import {
+  Button,
+  StyleSheet,
   Text,
   TextInput,
   TouchableHighlight,
-  StyleSheet,
-  View,
-  BackAndroid
+  View
 } from 'react-native';
+import axios from 'axios';
+import IconStyle1 from 'react-native-vector-icons/SimpleLineIcons';
+import IconStyle2 from 'react-native-vector-icons/MaterialIcons';
+import IconStyle3 from 'react-native-vector-icons/Entypo';
 
 const styles = StyleSheet.create({
-  pageContainer: {
+  welcomeContainer: {
     alignItems: 'center',
-    flex: 1,
+    backgroundColor: '#769ee2',
     flexDirection: 'column',
+    justifyContent: 'center',
+    height: 80
   },
-  icon: {
-    paddingRight: 15
+  appNameText: {
+    color: '#fff',
+    fontSize: 30
+  },
+  welcomeText: {
+    color: '#fff',
+    fontSize: 14
+  },
+  formContainer: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: 30,
+  },
+  inputLabel: {
+    color: '#393836',
+    fontSize: 17,
+    fontWeight: '600',
+    marginLeft: 15,
   },
   inputField: {
-    fontSize: 25,
+    fontSize: 17,
     height: 50,
-    width: 270
-  },
-  inputRow: {
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  submitButton: {
-    borderRadius: 6,
-    borderWidth: 1,
-    height: 44,
-    justifyContent: 'center',
-    marginTop: 20,
-    width: 80
-  },
+    marginLeft: 10,
+    marginBottom: 30,
+    width: 290
+  }
 });
+
+const userIcon = (<IconStyle1 name="user" size={18} color="black" />);
+const emailIcon = (<IconStyle2 name="email" size={20} color="black" />)
+const passwordIcon = (<IconStyle1 name="key" size={20} color="black" />);
+const addressIcon = (<IconStyle3 name="address" size={20} color="black" />);
 
 // BackAndroid.addEventListener("hardwareBackPress", () => {
 //   if (navigator.getCurrentRoutes().length > 1) {
@@ -54,71 +71,88 @@ export default class Registration extends Component {
       password: '',
       email: '',
       address: '',
-      imgUrl: ''
     };
 
     this.navigate = this.navigate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   navigate(routeName) {
     this.props.navigator.push({ name: routeName })
   }
 
+  handleSubmit() {
+    console.error(this.state);
+    // callApi(this.state);
+  }
+
   render() {
     return <View>
 
-        <View style={{alignSelf: 'stretch', alignItems: 'center', backgroundColor: '#FDD7E4', height: 90}}>
-          <Text style={styles.inputLabel}>Username</Text>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.appNameText}>
+          APP NAME
+        </Text>
+        <Text style={styles.welcomeText}>
+          Report. Review. Revamp.
+        </Text>
+      </View>
+
+      <View style={styles.formContainer}>
+
+          <View style={{ flexDirection: 'row' }}>{userIcon}<Text style={styles.inputLabel}>Username</Text></View>
           <TextInput
             name="username"
             style={styles.inputField}
             onChangeText={(username) => this.setState({username})}
             value={this.state.username}
           />
-        </View>
 
+          <View style={{ flexDirection: 'row' }}>{emailIcon}<Text style={styles.inputLabel}>Email</Text></View>
+          <TextInput
+            name="email"
+            style={styles.inputField}
+            onChangeText={(email) => this.setState({email})}
+            value={this.state.email}
+          />
 
-        <Text style={styles.inputLabel}>Password</Text>
-        <TextInput
-          name="password"
-          onChangeText={(password) => this.setState({password})}
-          secureTextEntry={true}
-          style={styles.inputField}
-          value={this.state.password}
-        />
+          <View style={{ flexDirection: 'row' }}>{passwordIcon}<Text style={styles.inputLabel}>Password</Text></View>
+          <TextInput
+            name="password"
+            onChangeText={(password) => this.setState({password})}
+            secureTextEntry={true}
+            style={styles.inputField}
+            value={this.state.password}
+          />
 
-        <Text style={styles.inputLabel}>Email</Text>
-        <TextInput
-          name="email"
-          style={styles.inputField}
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-        />
+          <View style={{ flexDirection: 'row' }}>{addressIcon}<Text style={styles.inputLabel}>Address</Text></View>
+          <TextInput
+            name="address"
+            style={styles.inputField}
+            onChangeText={(address) => this.setState({address})}
+            value={this.state.address}
+          />
 
-        <Text style={styles.inputLabel}>Address</Text>
-        <TextInput
-          name="address"
-          style={styles.inputField}
-          onChangeText={(address) => this.setState({address})}
-          value={this.state.address}
-        />
+          <View style={{ height: 10 }}></View>
 
-        <Text style={styles.inputLabel}>Image URL</Text>
-        <TextInput
-          name="imgUrl"
-          style={styles.inputField}
-          onChangeText={(imgUrl) => this.setState({imgUrl})}
-          value={this.state.imgUrl}
-        />
-
-        <TouchableHighlight
-          style={styles.submitButton}
-          onPress={() => this.navigate('map')}
-        >
-          <Text style={{alignItems: 'center'}}>Create Account</Text>
-        </TouchableHighlight>
-
-
+          <Button
+            color="#517cc6"
+            onPress={this.handleSubmit}
+            style={styles.submitButton}
+            title="Create Account"
+          />
+      </View>
     </View>
   }
+}
+
+function callApi(userInfo) {
+  axios
+    .post('https://q3project-server.herokuapp.com/api/users', userInfo)
+    .then((res) => {
+
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
 }
